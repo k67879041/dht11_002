b9f5fa1a69e826a2c96dc450cc54f9a60e6c3a95
ASUN <2679976429@qq.com>
2026-05-10 23:26:17 +0800

    更新文件

--- components/user_layout/user_layout.vue
+++ components/user_layout/user_layout.vue
@@ -1,6 +1,14 @@
 <template>
-	<view>
-		
+	<view class="user_layout">
+		<view class="header">header上层区域
+			<slot name="header"></slot>
+		</view>
+		<view class="main">main主体区域
+			<slot name="main"></slot>
+		</view>
+		<view class="footer">footer底部区域
+			<slot name="footer"></slot>
+		</view>
 	</view>
 </template>
 
@@ -9,5 +17,19 @@
 </script>
 
 <style lang="scss">
+.user_layout{
+	.header{
+ 	 height: 300rpx;
+ 	 background: #6f4b4b;
+	}
+	.main{
+	  height: 300rpx;
+	  background: #541d1d;
+	}
+	.footer{
+	  height: 300rpx;
+ 	 background: #c82424;
+	}
+}
 
 </style>
--- pages/demo5/demo5.vue
+++ pages/demo5/demo5.vue
@@ -1,13 +1,45 @@
 <template>
 	<view>
-		
+		<User_layout >
+			<template #header>
+				<view class="header">上层区域</view>
+			</template>
+			<template #main>
+				<view class="main">主体区域</view>
+			</template>
+			<template #footer>
+				<view class="footer">底部区域</view>
+			</template>
+		</User_layout>
 	</view>
 </template>
 
 <script setup>
-	
+	import User_layout from '@/components/user_layout/user_layout.vue';
+	import { ref,computed } from 'vue';
+	const shouming = ref('用户')
+	const shouming_computed = computed(() => {
+		return shouming.value + '的布局'
+	})
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.header{
+		height: 100rpx;
+		line-height: 100rpx;
+		text-align: center;
+		color: #fff;
+	}
+.main{
+		height: 200rpx;
+		line-height: 200rpx;
+		text-align: center;
+		color: #fff;
+	}
+.footer{
+		height: 100rpx;
+		line-height: 100rpx;
+		text-align: center;
+		color: #fff;
+	}
 </style>
